b3961036d3c03b4c20384f8b5d110c1d0999ca18
shreya713 <153702719+shreya713@users.noreply.github.com>
2023-12-21 12:06:10 +0530

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -11,7 +11,7 @@ const AllocationForm = (props) => {
     const submitEvent = () => {
 
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
+                alert("The value cannot exceed remaining funds  $"+remaining);
                 setCost("");
                 return;
             }
@@ -58,6 +58,9 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
+                  <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                <label className="input-group-text" htmlFor="inputGroupSelect02">$</label>
+                  </div>
 
                     <input
                         required='required'
