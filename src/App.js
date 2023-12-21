3c903cb19d8b06b681d9504da09d6f6357b039a0
shreya713 <153702719+shreya713@users.noreply.github.com>
2023-12-21 09:31:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
-import dropdown from './components/dropdown';
+import Dropdown from './components/dropdown';
 
 const App = () => {
     return (
